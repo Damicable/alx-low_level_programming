e6bca2ea63012a6c7aa7d029ad7a5ba3a0863d30
Damicable <annobidiegwu@gmail.com>
2023-03-23 19:37:37 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - A function that prints the largest number
+ * of a set of prime mumbers. using _putchar
+ * @n: The largest integer to print
+ *
+ * Return:
+ */
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+	n *= -1;
+	k = n;
+	_putchar('_');
+	}
+	k /= 10;
+	if (k != 0)
+	print_number(K);
+	_putchar((unsigned int) n % 10 + '0');
+
+}
