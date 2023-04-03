fe4839cd24dff2ee671469a64ab73b362259ae51
Damicable <annobidiegwu@gmail.com>
2023-04-03 16:17:10 -0700

    Location of character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - Write a function that locates a character in a string.
+ * @s: String
+ * @c: Element in th string
+ *
+ * Return: 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int 1 = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+
+}
